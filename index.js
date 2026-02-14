a5beb654faaf271e0d02a81a688a82abbc0fc7eb
IHearHowItIs <mike96vue@gmail.com>
2026-02-13 22:14:04 -0600

    fix picture sizes and names

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function changeDice(){
     var randomNumber1 = Math.random();
     randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
 
-    document.querySelector(".img1").setAttribute("src", "./images/picture" + randomNumber1 + ".png");
+    document.querySelector(".img1").setAttribute("src", "./images/Picture" + randomNumber1 + ".png");
 
     var randomNumber2 = Math.random();
     randomNumber2 = Math.floor(randomNumber2 * 6) + 1;
 
-    document.querySelector(".img2").setAttribute("src", "./images/picture" + randomNumber2 + ".png");
+    document.querySelector(".img2").setAttribute("src", "./images/Picture" + randomNumber2 + ".png");
 
     var winner = "";
     if(randomNumber1 > randomNumber2){
